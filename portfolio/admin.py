53db5ac98238d400ab5b430910f55494a81a7df5
Marco Fernandes <a22405693@alunos.ulht.pt>
2026-04-11 07:53:32 +0000

    Adicionado os modelos ao admin, junto com o passo 3 dos Passos Técnicos Detalhados

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Licenciatura)
+admin.site.register(UnidadeCurricular)
+admin.site.register(Projeto)
+admin.site.register(Tecnologia)
+admin.site.register(TFC)
+admin.site.register(Competencia)
+admin.site.register(Formacao)
+admin.site.register(MakingOf)
+admin.site.register(RegistoMakingOf)
+admin.site.register(ContribuicaoOpenSource)
